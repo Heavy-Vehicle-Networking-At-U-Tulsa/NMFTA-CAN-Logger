f8707de02420527d7daaebf76936456ef485f3e9
jeremy-daily <jeremy@synercontechnologies.com>
2016-09-30 00:11:50 -0500

    Added Licenses and Docs

--- SetTeensy3Time/SetTeensy3Time.ino
+++ SetTeensy3Time/SetTeensy3Time.ino
@@ -1,9 +1,34 @@
 /*
  * Set Time based on Serial Message from PC
  * 
+ * Written by Dr. Jeremy S. Daily
+ * The University of Tulsa
+ * Department of Mechanical Engineering
+ *
+ * Released under the MIT License
+ *
+ * Copyright (c) 2016        Jeremy S. Daily
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
  */
 
-#include <TimeLib.h>
+ *include <TimeLib.h>
 
 elapsedMillis displayCounter;
 
@@ -44,7 +69,7 @@ time_t getTeensy3Time()
 }
 
 /*  code to process time sync messages from the serial port   */
-#define TIME_HEADER  "T"   // Header tag for serial time sync message
+ *define TIME_HEADER  "T"   // Header tag for serial time sync message
 
 uint32_t processSyncMessage() {
   uint32_t pctime = 0L;
